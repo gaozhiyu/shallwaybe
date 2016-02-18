84f41788b5fd13b321be9c82ea713188adc0d3ee
gaozhiyu <gaozhiyu@79c3cf20-dd23-964d-9c9e-1fec1f826ead>
2016-02-18 14:28:08 +0000

    Add the logic of image download
    git-svn-id: file:///C/Users/Zongjiang/Dropbox/GoWithMiBE@11 79c3cf20-dd23-964d-9c9e-1fec1f826ead

---
+++ src/main/java/com/william/FileDownloadServlet.java
@@ -0,0 +1,51 @@
+package com.william;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class FileDownloadServlet extends HttpServlet {
+	
+	private final Logger logger =  Logger.getLogger(this.getClass());
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		doProcess(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		doProcess(request, response);
+	}
+
+	public void doProcess(HttpServletRequest request,
+			HttpServletResponse response) {
+		
+		try {
+			OutputStream out = response.getOutputStream();
+			FileInputStream in = new FileInputStream("d:/r001-001.jpg");
+			byte[] buffer = new byte[4096];
+			int length;
+			while ((length = in.read(buffer)) > 0){
+			    out.write(buffer, 0, length);
+			}
+			in.close();
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+ 
+
+
+
+	 
+}
