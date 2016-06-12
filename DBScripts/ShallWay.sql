b9562ce980002e90ab41ac9101407721884d23fb
gaozhiyu <gaozhiyu@79c3cf20-dd23-964d-9c9e-1fec1f826ead>
2016-06-12 09:10:01 +0000

    Create the functionality of calculating the distance
    git-svn-id: file:///C/Users/Zongjiang/Dropbox/GoWithMiBE@50 79c3cf20-dd23-964d-9c9e-1fec1f826ead

--- DBScripts/ShallWay.sql
+++ DBScripts/ShallWay.sql
@@ -1,19 +1,26 @@
-DROP TABLE IF EXISTS ShallWay;
-create table ShallWay (
-   ID VARCHAR(64) NOT NULL,	
-   ShallWayID VARCHAR(64) NOT NULL,
-   Country VARCHAR(128) default NULL,
-   City VARCHAR(128) default NULL,
-   Place VARCHAR(128) default NULL,
-   StartTime TIMESTAMP,
-   EndTime TIMESTAMP,
-   PostTime TIMESTAMP,
-   CarPool BOOLEAN default NULL,
-   FreeTour BOOLEAN default NULL,
-   HotelShare BOOLEAN default NULL,
-   FreeGuide BOOLEAN default NULL,
-   Title VARCHAR(512) default NULL,
-   Contact VARCHAR(128) default NULL,
-   Description BLOB default NULL,
-   PRIMARY KEY (ID)
-);
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS `calculateDistance`$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `calculateDistance`(lat1 double, lon1 double, lat2 double, lon2 double) RETURNS text CHARSET latin1
+BEGIN
+  DECLARE dLat double;
+ DECLARE dLon double;
+ DECLARE lat1r double;
+ DECLARE lat2r double;
+ DECLARE a double;
+ DECLARE b double;
+ DECLARE c double;
+ 
+  SET dLat = radians(lat2-lat1);
+  set dLon = radians(lon2-lon1);
+  set lat1r = radians(lat1);
+  set lat2r = radians(lat2);
+  set a = sin(dLat/2) * sin(dLat/2) +
+        sin(dLon/2) * sin(dLon/2) * cos(lat1r) * cos(lat2r); 
+  set b = 2 * atan2(sqrt(a), sqrt(1-a)); 
+  set c = 6371 * b;
+  return c;
+END$$
+
+DELIMITER ;
