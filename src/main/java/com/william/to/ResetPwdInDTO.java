d3208feb1675fcf9a6044dbd152da7f8cb3f0fed
gaozhiyu <gaozhiyu@79c3cf20-dd23-964d-9c9e-1fec1f826ead>
2016-07-10 05:56:45 +0000

    Add the logic of Reset password
    git-svn-id: file:///C/Users/Zongjiang/Dropbox/GoWithMiBE@66 79c3cf20-dd23-964d-9c9e-1fec1f826ead

---
+++ src/main/java/com/william/to/ResetPwdInDTO.java
@@ -0,0 +1,27 @@
+package com.william.to;
+
+public class ResetPwdInDTO {
+	private String username;
+	private String otp;
+	private String newPassword;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getOtp() {
+		return otp;
+	}
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	
+	
+}
