f8e4a443a7502b9e9d624a54611060f2908f61de
CMR-Products <CMR-Products@79c3cf20-dd23-964d-9c9e-1fec1f826ead>
2016-08-07 13:07:20 +0000

    Add JUnit Test for LatestCoordinate
    git-svn-id: file:///C/Users/Zongjiang/Dropbox/GoWithMiBE@88 79c3cf20-dd23-964d-9c9e-1fec1f826ead

---
+++ src/test/java/com/william/test/TestLatestCoordinate.java
@@ -0,0 +1,44 @@
+package com.william.test;
+
+import org.junit.Test;
+
+import com.william.DAO.LatestCoordinateDAO;
+import com.william.to.LatestCoordinateInDTO;
+import com.william.to.LatestCoordinateOutDTO;
+
+public class TestLatestCoordinate {
+	
+//  String lastShakeTimeString ="2016-04-05 12:30:00";
+//  SimpleDateFormat sdfd1 =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//  Date lastShakeTime =sdfd1.parse(lastShakeTimeString);
+  
+	LatestCoordinateInDTO latestCoordinateInDTO = new LatestCoordinateInDTO("9d39464ce8ce43a597acfe5e43c1c523","2.1", "2.2" ,"中国","南海省", "新加坡市");
+  
+	LatestCoordinateOutDTO latestCoordinateOutDTO= new LatestCoordinateOutDTO();
+	LatestCoordinateDAO MLC = new LatestCoordinateDAO();	
+	
+	@Test
+	public void TestSaveOrUpdateLatestCoordinate(){
+//      MLC.addLatestCoordinate(latestCoordinateInDTO);
+//      MLC.updateLatestCoordinate(latestCoordinateInDTO);
+		MLC.saveOrUpdateLatestCoordinate(latestCoordinateInDTO);		
+	}
+
+	@Test
+	public void TestReadLatestCoordinate(){
+		
+        latestCoordinateOutDTO = MLC.readLatestCoordinate("9d39464ce8ce43a597acfe5e43c1c523");
+        
+		System.out.println();
+		System.out.println("Nickname: "+latestCoordinateOutDTO.getNickname());
+		System.out.println("UserIntID: "+latestCoordinateOutDTO.getUserIntID());
+		System.out.println("\tLongitude: "+latestCoordinateOutDTO.getLongitude());
+		System.out.println("\tLatitude: "+latestCoordinateOutDTO.getLatitude());
+		System.out.println("\tLast Shake Time: "+latestCoordinateOutDTO.getLastShakeTime());
+		System.out.println("\tCountry: "+latestCoordinateOutDTO.getCountry());
+		System.out.println("\tProvince: "+latestCoordinateOutDTO.getProvince());
+		System.out.println("\tCity: "+latestCoordinateOutDTO.getCity());
+		System.out.println("\tLast Address Update: "+latestCoordinateOutDTO.getLastAddressUpdate());
+	
+	}
+}
